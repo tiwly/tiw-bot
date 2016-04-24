11a69300917886b15e556bd76f24f095a58a98a5
Tiwly Twin <macintosh@Macintoshs-MacBook-Pro.local>
2016-04-24 14:28:59 +0700

    dsadsa

--- tiw-bot.js
+++ tiw-bot.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var app = express()
+var token = "CAAOvJWRjvZAwBAKzv9egmvEVwf0dGN6CiCZA6bcSybChtDUnm8Kr8Sgpw0DwaZADHZCXnjaWZAO4X2pjpDfmwxGZBvO7mExVYuLUD8M2ebGIiDK2pxuymRrU5d78vSPA1WZC9rSrtmnUKyaxZC2oKaLi00yNPjoIQWcrSakloKKBqqZAMLhBtwiRFI8WLAJGgJ8fX8kn7D6ybywZDZD";
+var request = require('request')
 app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
@@ -19,9 +21,10 @@ app.post('/webhook/', function (req, res) {
     var event = req.body.entry[0].messaging[i];
     var sender = event.sender.id;
     if (event.message && event.message.text) {
-    	vartext = event.message.text;
+    	var text = event.message.text;
     	// Handle a text message from this sender
     	console.log(text);
+    	sendTextMessage(sender,text);
     }
   }
   res.sendStatus(200);
@@ -32,3 +35,24 @@ app.set('port', (process.env.PORT || 5000))
 app.listen(app.get('port'), function () {
   console.log('Example aoo listening on port ' + app.get('port') + ' !')
 })
+
+function sendTextMessage(sender, text) {
+  var messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
