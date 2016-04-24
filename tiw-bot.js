0ca8b00d3da36bd3939dc1cd45a6e9f65c4bc6dc
Tiwly Twin <macintosh@Macintoshs-MacBook-Pro.local>
2016-04-24 13:42:55 +0700

    addignore

--- tiw-bot.js
+++ tiw-bot.js
@@ -5,6 +5,13 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === '1234') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
+
 app.set('port',(process.env.PORT || 5000));
 
 app.listen(app.get('port'),function{
