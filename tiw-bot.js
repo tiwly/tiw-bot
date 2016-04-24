172b1f2afd02a505faccd2f4588a9b7dc2b57599
Tiwly Twin <macintosh@Macintoshs-MacBook-Pro.local>
2016-04-24 14:10:29 +0700

    dsadsa

--- tiw-bot.js
+++ tiw-bot.js
@@ -12,6 +12,20 @@ app.get('/webhook/', function (req, res) {
   res.send('Error, wrong validation token')
 })
 
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+    	text = event.message.text;
+    	// Handle a text message from this sender
+    	console.log(text);
+    }
+  }
+  res.sendStatus(200);
+});
+
 app.set('port', (process.env.PORT || 5000))
 
 app.listen(app.get('port'), function () {
